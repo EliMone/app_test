ffbf8d2c867853b072d88933f54f1330eecedc5f
em1404 <em1404@messiah.edu>
2019-02-09 18:12:39 -0500

    with api set up.

--- private_html/dbconfig.php
+++ private_html/dbconfig.php
@@ -1,1 +1,43 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>api setup</title>
+   
+</head>
+<body>
+    
+
+<script type="text/javascript">
+       
+       
+        const client_id = "0805466082cde7109541";
+        const client_secret = "3ea462e3e127bb0bda551c4d8106b9f7c0947419";
+        const fetchUsers = async () => {
+            const api_call = await fetch('https://api.github.com/users/em1404?client_id=${client_id}&client_secret=${client_secret}');
+        
+            const data = await api_call.json();
+            return {data}
+        };
+
+        const showData = () => {
+            fetchUsers().then((res) => {
+                console.log(res)
+
+                document.getElementById("user").innerHTML = res.data.login;
+               
+            })
+        };
+        
+        showData();
+
+        
+
+    </script>
+
+    <p id="user"></p>
+
+     
+</body>
+</html>
